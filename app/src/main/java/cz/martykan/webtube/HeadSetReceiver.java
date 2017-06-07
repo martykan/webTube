58fe7d0559300bee4a90e0639f32b6ef126a4b8e
korok <korok@debi>
2017-06-07 17:58:51 +0200

    Add unplug headphone detection

---
+++ app/src/main/java/cz/martykan/webtube/HeadSetReceiver.java
@@ -0,0 +1,25 @@
+package cz.martykan.webtube;
+
+import android.content.BroadcastReceiver;
+import android.content.*;
+import android.util.Log;
+
+public class HeadSetReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_HEADSET_PLUG)) {
+            int state = intent.getIntExtra("state", -1);
+            switch (state) {
+                case 0:
+                    MainActivity.pauseVideo();
+                    Log.i("Value", "Headset unplugged");
+                    break;
+                case 1:
+                    Log.i("Value", "Headset plugged");
+                    break;
+            }
+        }
+    }
+
+}
